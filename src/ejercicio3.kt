83bcbace3b9e0fd19215ae039d5248cf01b84536
ilopjim13 <ljivan@iesrafaelalberti.es>
2024-01-22 21:34:32 +0100

    seguido desde clase 2

--- src/ejercicio3.kt
+++ src/ejercicio3.kt
@@ -1,14 +1,14 @@
 import java.lang.IllegalArgumentException
 
 class Rectangulo(base:Double, altura:Double) {
-    private var base:Double = 0.0
+    var base:Double = 0.0
         set(value) {
-            require(value>=0) { "La base no puede ser 0" }
+            require(value>0) { "La base no puede ser 0" }
             field = value
         }
-    private var altura:Double = 0.0
+    var altura:Double = 0.0
         set(value) {
-            require(value>=0) { "La altura no puede ser 0" }
+            require(value>0) { "La altura no puede ser 0" }
             field = value
         }
     init {
@@ -16,13 +16,11 @@ class Rectangulo(base:Double, altura:Double) {
         this.altura = altura
     }
 
-    private fun area(): Double {
-        return this.base * this.altura
-    }
+    private fun area() = this.base * this.altura
 
-    private fun perimetro(): Double {
-        return (this.base * 2) + (this.altura * 2)
-    }
+
+    private fun perimetro() =  (this.base * 2) + (this.altura * 2)
+
 
     override fun toString(): String {
         return "El área del rectangulo es de ${String.format("%.2f", area())}cm y su perímetro de ${String.format("%.2f", perimetro())}cm"
@@ -33,7 +31,7 @@ fun main() {
     try {
         val rect1 = Rectangulo(3.7,5.4)
         println("Rectangulo 1:")
-        println(rect1.toString())
+        println(rect1)
 
         val rect2 = Rectangulo(4.2,3.8)
         println("\nRectangulo 2:")
